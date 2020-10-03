d4e8ac60ece1ad4922e98fac215fc4ca3f92e030
Mikko Saari <mikko@mikkosaari.fi>
2020-10-03 09:07:58 +0300

    First commit

---
+++ relevanssi-premium-snowball-stemmer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Relevanssi Premium Snowball Stemmer
+ *
+ * /relevanssi-premium-snowball-stemmer.php
+ *
+ * @package Relevanssi Premium Snowball Stemmer
+ * @author  Mikko Saari
+ * @license https://wordpress.org/about/gpl/ GNU General Public License
+ * @see     https://www.relevanssi.com/
+ *
+ * @wordpress-plugin
+ * Plugin Name: Relevanssi Premium Snowball Stemmer
+ * Plugin URI: https://www.relevanssi.com/
+ * Description: This plugin adds Snowball Stemmer for Relevanssi Premium.
+ * Version: 0.1
+ * Author: Mikko Saari
+ * Author URI: http://www.mikkosaari.fi/
+ * Text Domain: relevanssi
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Does the actual stemming. Gets the language from the option
+ * `relevanssi_premium_stemmer_language`.
+ *
+ * @param string $word The word to stem.
+ *
+ * @return string The stemmed word.
+ */
+function relevanssi_premium_snowball_stemmer( $word ) {
+	require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+	$language = get_option( 'relevanssi_premium_stemmer_language', 'en' );
+	try {
+		$stemmer = Wamania\Snowball\StemmerFactory::create( $language );
+	} catch ( Wamania\Snowball\NotFoundException $e ) {
+		return $word;
+	}
+
+	$stemmed_word = $stemmer->stem( $word );
+	if ( is_string( $stemmed_word ) ) {
+		return $stemmed_word;
+	}
+
+	// User asked for a non-existing language or some other error happened.
+	return $word;
+}
