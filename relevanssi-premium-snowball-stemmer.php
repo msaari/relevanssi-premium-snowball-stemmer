7365b81f7a8314c109bca7cf3dd924e144a12106
Mikko Saari <mikko@mikkosaari.fi>
2020-10-05 13:39:04 +0300

    Version number bump to 1.0

--- relevanssi-premium-snowball-stemmer.php
+++ relevanssi-premium-snowball-stemmer.php
@@ -13,7 +13,7 @@
  * Plugin Name: Relevanssi Premium Snowball Stemmer
  * Plugin URI: https://www.relevanssi.com/snowball-stemmer/
  * Description: This plugin adds Snowball Stemmer for Relevanssi Premium.
- * Version: 0.1
+ * Version: 1.0
  * Author: Mikko Saari
  * Author URI: http://www.mikkosaari.fi/
  * Text Domain: relevanssi
